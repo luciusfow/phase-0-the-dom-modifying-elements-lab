a85eaa07ca0aa2bb6ec96d4238e21d369a8c6edd
Lucius Fowler <luciusfow@gmail.com>
2021-05-18 20:36:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+main.remove()
+
+
+// const newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader); 
+
+// const newheader = document.getElementById('h1')
+// newHeader.innerHTML = "<h1>victory</h1>"
+// element.appendChild(body)
+
+// const newHeader = document.createElement('H1')
+// const text = document.createTextNode('victory')
+// newHeader.appendChild(text)
+
+const newHeader = document.createElement('H1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Ian is the champion'
